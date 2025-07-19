458db743644c2a761e169a35b30dd2b354f5d40a
AzeezSWE <im.nomanoff@gmail.com>
2025-07-19 22:59:10 +0900

    Topic: React Router Part 22222

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -2,27 +2,41 @@
 import { useNavigate } from "react-router";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import PATHS from "../routes/routes";
+
+const articles = [
+  {
+    postDate: "2020-01-01",
+    postTitle: "Koreaga o'qishga topshirish yanada osonlashdi",
+    postContent: "Bluh blah blah",
+  },
+  {
+    postDate: "2021-03-03",
+    postTitle: "Germaniya o'qishga topshirish yanada osonlashdi",
+    postContent: "Bluh blah blah",
+  },
+  {
+    postDate: "2022-05-05",
+    postTitle: "Yaponiya o'qishga topshirish yanada osonlashdi",
+    postContent: "Bluh blah blah",
+  },
+  {
+    postDate: "2023-07-07",
+    postTitle: "Kanada o'qishga topshirish yanada osonlashdi",
+    postContent: "Bluh blah blah",
+  },
+  {
+    postDate: "2024-09-09",
+    postTitle: "AQSH o'qishga topshirish yanada osonlashdi",
+    postContent: "Bluh blah blah",
+  },
+];
+
 function News() {
   const navigate = useNavigate();
-
-  const handleNavigateArticleOne = () => {
-    const article1 = {
-      postDate: "2020-01-01",
-      postTitle: "Koreaga o'qishga topshirish yanada osonlashdi",
-      postContent: "Bluh blah blah",
-    };
-
-    navigate(`/news/${article1.postDate}/${article1.postTitle}`);
-  };
-
-  const handleNavigateArticleTwo = () => {
-    const article2 = {
-      postDate: "2021-03-03",
-      postTitle: "Germaniya o'qishga topshirish yanada osonlashdi",
-      postContent: "Bluh blah blah",
-    };
-
-    navigate(`/news/${article2.postDate}/${article2.postTitle}`);
+  const handleViewDeatails = (title, date) => {
+    navigate(
+      `${PATHS.NEWS_DETAIL.replace(":id", date).replace(":title", title)}`
+    );
   };
 
   return (
@@ -37,11 +51,19 @@ function News() {
       <h1>News Page</h1>
       <p>This is the news page of our application.</p>
       <ul>
-        <li onClick={handleNavigateArticleOne}>Article 1</li>
-        <li onClick={handleNavigateArticleTwo}>Article 2</li>
-        <li>Article 3</li>
-        <li>Article 4</li>
-        <li>Article 5</li>
+        {articles.map((article, index) => (
+          <li key={index}>
+            <h2>{article.postTitle.slice(0, 5)}</h2>
+            <p>{article.postDate}</p>
+            <button
+              onClick={() =>
+                handleViewDeatails(article.postTitle, article.postDate)
+              }
+            >
+              Read More
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
