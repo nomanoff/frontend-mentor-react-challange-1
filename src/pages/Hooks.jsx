4dc094df80632daaea3250199fb3ea0ece1bedb9
AzeezSWE <im.nomanoff@gmail.com>
2025-07-22 22:49:14 +0900

    useEffect hook

--- src/pages/Hooks.jsx
+++ src/pages/Hooks.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router";
 import PATHS from "../routes/routes";
 import { Button } from "@mui/material";
 
 function Hooks() {
   let navigate = useNavigate();
+  const inputRef = useRef(null);
   const [username, setUserName] = useState("");
+
+  useEffect(() => {
+    // focus the input
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  });
+
+  const handleInputFocus = () => {};
 
   return (
     <div>
@@ -21,12 +31,17 @@ function Hooks() {
 
       <div>
         <input
+          ref={inputRef}
           id="username"
           value={username}
           onChange={(e) => setUserName(e.target.value)}
         />
 
         <button>Login</button>
+      </div>
+
+      <div>
+        <button onClick={handleInputFocus}>focus input</button>
       </div>
     </div>
   );
